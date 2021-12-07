74f582390200afd6435ba86a73bb851c7b987029
huynd1 <huynd1@runsystem.net>
2021-12-07 21:33:24 +0700

    update image

--- instagramiOS/ContentView.swift
+++ instagramiOS/ContentView.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+let Iris = Color(red: 0.317, green: 0.356, blue: 0.831)
+let Grape = Color(red: 0.505, green: 0.203, blue: 0.686)
+let Vivid = Color(red: 0.866, green: 0.164, blue: 0.482)
+let Princeton = Color(red: 0.96, green: 0.521, blue: 0.16)
 
 struct ContentView: View {
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+        VStack {
+            VStack(){
+                Image("Instagram_logo_white").resizable()
+                    .frame(width: 250, height: 100)
+                  }
+                .padding(40)
+            
+                Divider()
+                Spacer()
+            }
+            .background(LinearGradient(gradient: Gradient(colors: [Iris, Grape, Vivid, Princeton]), startPoint: .bottomTrailing, endPoint: .topLeading))
+            .ignoresSafeArea(.all)
     }
 }
 
